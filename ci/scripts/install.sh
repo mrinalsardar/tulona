88de8fe0a5154f21f5c8e2f061b6e004a2f6de14
Mrinal Kanti Sardar <mrinal.k.sardar@gmail.com>
2023-09-23 10:38:48 +0000

    Remove everything related to CI for a fresh start

--- ci/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-
-# Install tulona
-python -m pip install --quiet --no-deps -e ./core
-
-echo mamba list
-mamba list
--- ci/scripts/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Run test with coverage"
-coverage run -m pytest -v tests
-
-echo "Generate coverage report[XML]"
-coverage xml
