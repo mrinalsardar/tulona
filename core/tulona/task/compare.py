0f09f23fbc1701291a2be39319dce46fae548ebb
Mrinal Kanti Sardar <mrinal.k.sardar@gmail.com>
2024-04-08 23:12:00 +0530

    Added row comparison to 'compare' command

--- core/tulona/task/compare.py
+++ core/tulona/task/compare.py
@@ -8,7 +8,7 @@ import pandas as pd
 from tulona.config.runtime import RunConfig
 from tulona.exceptions import TulonaMissingPrimaryKeyError, TulonaMissingPropertyError
 from tulona.task.base import BaseTask
-from tulona.task.helper import create_profile, perform_comparison
+from tulona.task.helper import create_profile, extract_rows, perform_comparison
 from tulona.util.dataframe import apply_column_exclusion
 from tulona.util.excel import highlight_mismatch_cells
 from tulona.util.filesystem import get_outfile_fqn
@@ -19,6 +19,7 @@ from tulona.util.sql import (
     get_column_query,
     get_query_output_as_df,
     get_sample_row_query,
+    get_table_fqn,
 )
 
 log = logging.getLogger(__name__)
@@ -86,8 +87,16 @@ class CompareDataTask(BaseTask):
         dbtype2 = self.profile["profiles"][
             extract_profile_name(self.project, datasource2)
         ]["type"]
-        table_name1 = extract_table_name_from_config(config=ds_dict1, dbtype=dbtype1)
-        table_name2 = extract_table_name_from_config(config=ds_dict2, dbtype=dbtype2)
+        table_name1 = get_table_fqn(
+            ds_dict1["database"] if dbtype1 != "mysql" else None,
+            ds_dict1["schema"],
+            ds_dict1["table"],
+        )
+        table_name2 = get_table_fqn(
+            ds_dict2["database"] if dbtype2 != "mysql" else None,
+            ds_dict2["schema"],
+            ds_dict2["table"],
+        )
 
         # Extract rows from both data sources
         log.debug(
@@ -160,9 +169,13 @@ class CompareDataTask(BaseTask):
         # Exclude columns
         log.debug("Excluding columns")
         if "exclude_columns" in ds_dict1:
-            df1 = apply_column_exclusion(df1, ds_dict1, table_name1)
+            df1 = apply_column_exclusion(
+                df1, ds_dict1["primary_key"], ds_dict1["exclude_columns"], table_name1
+            )
         if "exclude_columns" in ds_dict2:
-            df2 = apply_column_exclusion(df2, ds_dict2, table_name2)
+            df2 = apply_column_exclusion(
+                df2, ds_dict2["primary_key"], ds_dict2["exclude_columns"], table_name2
+            )
 
         # Compare
         common_columns = list(
@@ -306,7 +319,7 @@ class CompareColumnTask(BaseTask):
             left_on=column1,
             right_on=column2,
             how="outer",
-            suffixes=["_left_" + ds1_compressed, "_right_" + ds2_compressed],
+            suffixes=("_left_" + ds1_compressed, "_right_" + ds2_compressed),
             validate="one_to_one",
             indicator="presence",
         )
@@ -346,12 +359,92 @@ class CompareTask(BaseTask):
         log.info("Starting task: compare")
         start_time = time.time()
 
-        if len(self.datasources) < 2:
-            raise ValueError("Comparison needs at least two data sources.")
+        if len(self.datasources) != 2:
+            raise ValueError("Comparison needs two data sources.")
+
+        # ------- Row comparison: different beast | against DRY principle
+        # TODO: Add support of composite primary key
+        # TODO: Add support for different names of primary keys in different tables
+        # Check if primary key[s] is[are] specified for row comparison
+        primary_keys = set()
+        for ds_name in self.datasources:
+            # Extract data source name from datasource:column combination
+            ds_name = ds_name.split(":")[0]
+            ds_config = self.project["datasources"][ds_name]
+            if "primary_key" in ds_config:
+                if (
+                    isinstance(ds_config["primary_key"], list)
+                    and len(ds_config["primary_key"]) > 1
+                ):
+                    raise ValueError("Composite primary key is not supported yet")
+                primary_keys = primary_keys.union({ds_config["primary_key"]})
+
+        if len(primary_keys) == 0:
+            raise ValueError(
+                "Primary key must be provided with at least one of the data source config"
+            )
+
+        if len(primary_keys) > 1:
+            raise ValueError(
+                "Primary key column name has to be same in all candidate tables for comparison"
+            )
+        primary_key = primary_keys.pop()
+
+        # Config extraction
+        ds1_name = self.datasources[0].split(":")[0]
+        ds1_config = self.project["datasources"][ds1_name]
+        dbtype1 = self.profile["profiles"][extract_profile_name(self.project, ds1_name)][
+            "type"
+        ]
+        table_fqn1 = get_table_fqn(
+            ds1_config["database"] if "database" in ds1_config else None,
+            ds1_config["schema"],
+            ds1_config["table"],
+        )
+        connection_profile1 = get_connection_profile(self.profile, self.project, ds1_name)
+        conman1 = self.get_connection_manager(conn_profile=connection_profile1)
+        exclude_columns1 = (
+            ds1_config["exclude_columns"] if "exclude_columns" in ds1_config else []
+        )
+        if isinstance(exclude_columns1, str):
+            exclude_columns1 = [exclude_columns1]
+
+        ds2_name = self.datasources[1].split(":")[0]
+        ds2_config = self.project["datasources"][ds2_name]
+        dbtype2 = self.profile["profiles"][extract_profile_name(self.project, ds2_name)][
+            "type"
+        ]
+        table_fqn2 = get_table_fqn(
+            ds2_config["database"] if "database" in ds2_config else None,
+            ds2_config["schema"],
+            ds2_config["table"],
+        )
+        connection_profile2 = get_connection_profile(self.profile, self.project, ds2_name)
+        conman2 = self.get_connection_manager(conn_profile=connection_profile2)
+        exclude_columns2 = (
+            ds2_config["exclude_columns"] if "exclude_columns" in ds2_config else []
+        )
+        if isinstance(exclude_columns2, str):
+            exclude_columns2 = [exclude_columns2]
+
+        log.info("Extracting row data")
+        row_data_list = extract_rows(
+            dbtype1=dbtype1,
+            table_fqn1=table_fqn1,
+            conman1=conman1,
+            exclude_columns1=exclude_columns1,
+            dbtype2=dbtype2,
+            table_fqn2=table_fqn2,
+            conman2=conman2,
+            exclude_columns2=exclude_columns2,
+            primary_key=primary_key,
+            sample_count=self.sample_count,
+        )
 
         # --------------- Data collection from sources
-        df_collection = []
         ds_name_compressed_list = []
+        profile_list = []
+        # column_data_list = []
         for ds_name in self.datasources:
             log.debug(f"Extracting configs for: {ds_name}")
             # Extract data source name from datasource:column combination
@@ -379,7 +472,7 @@ class CompareTask(BaseTask):
             conman = self.get_connection_manager(conn_profile=connection_profile)
 
             # Profile data
-            log.info(f"Profiling {ds_name}")
+            log.info(f"Extracting profile data for {ds_name}")
             metrics = [
                 "min",
                 "max",
@@ -388,28 +481,36 @@ class CompareTask(BaseTask):
                 "distinct_count",
             ]
             df = create_profile(database, schema, table, metrics, conman)
-            df_collection.append(df)
-
-            # Row data
+            profile_list.append(df)
 
             # Column data
 
         # --------------- Comparison
         comparisons = {}
 
+        # Row data comparison
+        log.debug("Preparing row data comparison")
+        df_row_comp = perform_comparison(
+            ds_name_compressed_list, row_data_list, primary_key
+        )
+        comparisons["Row Comparison"] = {
+            "primary_key": primary_key.lower(),
+            "data": df_row_comp,
+            "num_sources": len(row_data_list),
+        }
+        log.debug(f"Prepared comparison for {df_row_comp.shape[0]} rows")
+
         # Profile comparison
         log.debug("Preparing metadata comparison")
         df_profiles = perform_comparison(
-            ds_name_compressed_list, df_collection, "column_name"
+            ds_name_compressed_list, profile_list, "column_name"
         )
-        comparisons["profiles"] = {
+        comparisons["Metadata Comparison"] = {
             "primary_key": "column_name",
             "data": df_profiles,
             "num_sources": len(ds_name_compressed_list),
         }
         log.debug(f"Prepared comparison for {df_profiles.shape[0]} columns")
-
-        # Row comparison
 
         # Column comparison
 
--- core/tulona/task/helper.py
+++ core/tulona/task/helper.py
@@ -1,10 +1,18 @@
 import logging
-from typing import List
+from typing import List, Union
 
 import pandas as pd
 
 from tulona.adapter.connection import ConnectionManager
-from tulona.util.sql import get_metadata_query, get_metric_query, get_query_output_as_df
+from tulona.util.dataframe import apply_column_exclusion
+from tulona.util.sql import (
+    build_filter_query_expression,
+    get_metadata_query,
+    get_metric_query,
+    get_query_output_as_df,
+    get_table_data,
+    get_table_fqn,
+)
 
 log = logging.getLogger(__name__)
 
@@ -21,7 +29,7 @@ def create_profile(
 
     # Extract metrics like min, max, avg, count, distinct count etc.
     log.debug("Extracting metrics")
-    # metrics = ["count", "distinct_count"]
+    table_fqn = get_table_fqn(database, schema, table)
     metrics = list(map(lambda s: s.lower(), metrics))
     type_dict = df_meta[["column_name", "data_type"]].to_dict(orient="list")
     columns_dtype = {
@@ -31,18 +39,16 @@ def create_profile(
     # TODO: quote for columns should be a config option, not an arbitrary thing
     try:
         log.debug("Trying query with unquoted column names")
-        metric_query = get_metric_query(database, schema, table, columns_dtype, metrics)
+        metric_query = get_metric_query(table_fqn, columns_dtype, metrics)
         log.debug(f"Executing query: {metric_query}")
         df_metric = get_query_output_as_df(
             connection_manager=conman, query_text=metric_query
         )
     except Exception as exp:
-        log.warn(f"Previous query failed with error: {exp}")
+        log.warning(f"Previous query failed with error: {exp}")
         log.debug("Trying query with quoted column names")
         metric_query = get_metric_query(
-            database,
-            schema,
-            table,
+            table_fqn,
             columns_dtype,
             metrics,
             quoted=True,
@@ -70,13 +76,77 @@ def create_profile(
     return df
 
 
+def extract_rows(
+    dbtype1: str,
+    table_fqn1: str,
+    conman1: ConnectionManager,
+    exclude_columns1: List[str],
+    dbtype2: str,
+    table_fqn2: str,
+    conman2: ConnectionManager,
+    exclude_columns2: List[str],
+    primary_key: str,
+    sample_count: int,
+) -> Union[List[pd.DataFrame], None]:
+    # TODO: push column exclusion down to the database/query
+
+    primary_key = primary_key.lower()
+    query_expr = None
+
+    i = 0
+    while i < 5:
+        log.debug(f"Extraction iteration: {i + 1}")
+
+        df1 = get_table_data(conman1, dbtype1, table_fqn1, sample_count, query_expr)
+        if df1.shape[0] == 0:
+            raise ValueError(f"Table {table_fqn1} doesn't have any data")
+
+        df1 = df1.rename(columns={c: c.lower() for c in df1.columns})
+        if primary_key not in df1.columns.tolist():
+            raise ValueError(f"Primary key {primary_key} not present in {table_fqn2}")
+
+        # Exclude columns
+        log.debug(f"Excluding columns from {table_fqn1}")
+        if len(exclude_columns1):
+            df1 = apply_column_exclusion(df1, primary_key, exclude_columns1, table_fqn1)
+
+        df2 = get_table_data(
+            conman2,
+            dbtype2,
+            table_fqn2,
+            sample_count,
+            query_expr=build_filter_query_expression(df1, primary_key),
+        )
+        df2 = df2.rename(columns={c: c.lower() for c in df2.columns})
+
+        if primary_key not in df2.columns.tolist():
+            raise ValueError(f"Primary key {primary_key} not present in {table_fqn2}")
+
+        # Exclude columns
+        log.debug(f"Excluding columns from {table_fqn2}")
+        if len(exclude_columns2):
+            df2 = apply_column_exclusion(df2, primary_key, exclude_columns2, table_fqn2)
+
+        if df2.shape[0] > 0:
+            df1 = df1[df1[primary_key].isin(df2[primary_key].tolist())]
+            return [df1, df2]
+        else:
+            query_expr = build_filter_query_expression(df1, primary_key, positive=False)
+
+        i += 1
+
+    raise ValueError(f"Could not find common data between {table_fqn1} and {table_fqn2}")
+
+
 # TODO: common param to toggle comparison result for common vs all columns
 def perform_comparison(
     ds_compressed_names: List[str], dataframes: List[pd.DataFrame], primary_key: str
 ) -> pd.DataFrame:
+    primary_key = primary_key.lower()
     common_columns = {c.lower() for c in dataframes[0].columns.tolist()}
+
     dataframes_final = []
-    for ds_name, df in zip(ds_compressed_names, dataframes):
+    for df in dataframes[1:]:
         colset = {c.lower() for c in df.columns.tolist()}
         common_columns = common_columns.intersection(colset)
 
@@ -88,7 +158,8 @@ def perform_comparison(
                 for c in df.columns
             }
         )
-        df[primary_key] = df[primary_key].str.lower()
+        if pd.api.types.is_string_dtype(df[primary_key]):
+            df[primary_key] = df[primary_key].str.lower()
         dataframes_final.append(df)
 
     df_merge = dataframes_final.pop()
