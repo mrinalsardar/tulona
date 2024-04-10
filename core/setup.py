a456a3feaed34f04dc363fb7927185f5032388ec
Mrinal Kanti Sardar <mrinal.k.sardar@gmail.com>
2024-04-10 23:30:20 +0530

    Bump version: 0.3.1 → 0.3.2

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 if __name__ == "__main__":
     setup(
         name="tulona",
-        version="0.3.1",
+        version="0.3.2",
         packages=find_namespace_packages(include=["tulona", "tulona.*"]),
         include_package_data=True,
         entry_points={
