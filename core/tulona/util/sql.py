86285f56e0085e7194cd0e6e903bec82ff57d023
Mrinal Kanti Sardar <mrinal.k.sardar@gmail.com>
2024-04-11 00:00:07 +0530

    tests for sql util

--- core/tulona/util/sql.py
+++ core/tulona/util/sql.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from typing import Dict, Union
 
 import pandas as pd
 
 from tulona.exceptions import TulonaNotImplementedError
 
 
-def get_table_fqn(database: str, schema: str, table: str) -> str:
+def get_table_fqn(database: Union[str, None], schema: str, table: str) -> str:
     table_fqn = f"{database + '.' if database else ''}{schema}.{table}"
     return table_fqn
 
@@ -168,7 +168,7 @@ def get_metric_query(table_fqn, columns_dtype: Dict, metrics: list, quoted=False
     return query
 
 
-def get_table_data_query(conman, dbtype, table_fqn, sample_count, query_expr: str = None):
+def get_table_data_query(dbtype, table_fqn, sample_count, query_expr: str = None):
     if query_expr:
         query = f"select * from {table_fqn} where {query_expr}"
     else:
